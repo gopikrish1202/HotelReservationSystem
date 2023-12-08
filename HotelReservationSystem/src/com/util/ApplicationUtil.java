091d031625323a98c3253f26dde2fb421294a3ea
gopikrish1202 <68181485+gopikrish1202@users.noreply.github.com>
2023-12-08 23:57:09 +0530

    Update ApplicationUtil.java

--- HotelReservationSystem/src/com/util/ApplicationUtil.java
+++ HotelReservationSystem/src/com/util/ApplicationUtil.java
@@ -5,13 +5,23 @@ import java.util.Collection;
 
 public class ApplicationUtil {
 
-	public List<String> extractDetails(String[] input) {
+	public List<String> extractDetailsRI(String[] input) {
 		// TODO Auto-generated method stub
 		List<String> riRecords=new ArrayList<String>();
 		for(int i=0;i<input.length;i++) {
 			riRecords.add(input[i]);
 		}
 		return riRecords;
+	}
+	
+	
+	public List<String> extractDetailsNRI(String[] input) {
+		// TODO Auto-generated method stub
+		List<String> NriRecords=new ArrayList<String>();
+		for(int i=0;i<input.length;i++) {
+			NriRecords.add(input[i]);
+		}
+		return NriRecords;
 	}
 	
 }
