02898b6c76b63ceef7c50f71e590bd6a88519dfe
gopikrish1202 <68181485+gopikrish1202@users.noreply.github.com>
2023-12-09 13:42:19 +0530

    Delete HotelReservationSystem/hotel_reservation_system directory

--- HotelReservationSystem/hotel_reservation_system/room.sql
+++
@@ -1,9 +0,0 @@
-create table room(
-    room_number varchar(25) PRIMARY KEY,
-    floor_number varchar(25),
-    is_occupied varchar(25),
-    max_occupancy int,
-    has_ac varchar(25),
-    has_swimming_pool_access varchar(25),
-    has_gym_access varchar(25)
-    );
